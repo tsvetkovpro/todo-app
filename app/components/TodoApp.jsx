66049f780787942c7ea2f0719f397246ee312636
tsvetkovpro <285019@gmail.com>
2017-01-06 21:35:57 +0500

    init

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,50 @@
+var React = require ('react');
+var TodoList = require ('TodoList');
+var AddTodo = require ('AddTodo');
+var TodoSearch = require ('TodoSearch');
+
+var TodoApp = React.createClass ({
+	getInitialState: function () {
+		return {
+			showCompleted: false,
+			searchText: '',
+			todos: [
+				{
+					id: 1,
+					text: 'Walk the dog'
+				}, {
+					id: 2,
+					text: 'Clean the yard'
+				}, {
+					id: 3,
+					text: 'Leave mail on porch'
+				}, {
+					id: 4,
+					text: 'Play video games'
+				}
+			]
+		};
+	},
+	handleAddTodo: function (text) {
+		console.log ('new todo: ' + text);
+	},
+	handleSearch: function (showCompleted, searchText) {
+		this.setState({
+			showCompleted: showCompleted,
+			searchText: searchText.toLowerCase()
+		});
+	},
+	render: function () {
+		var {todos} = this.state;
+
+		return (
+			<div>
+				<TodoSearch onSearch={this.handleSearch}/>
+				<TodoList todos={todos}/>
+				<AddTodo onAddTodo={this.handleAddTodo}/>
+			</div>
+		)
+	}
+});
+
+module.exports = TodoApp;
