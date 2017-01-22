b27ce9513fc11382145cae6ca0366799ef148623
tsvetkovpro <285019@gmail.com>
2017-01-22 16:09:01 +0500

    production env

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
 	var config = {
-		apiKey: "AIzaSyDbx09ufN7LTCCB77ui_StM5u7gsxp_i5c",
-		authDomain: "todo-app-56.firebaseapp.com",
-		databaseURL: "https://todo-app-56.firebaseio.com",
-		storageBucket: "todo-app-56.appspot.com",
-		messagingSenderId: "200145457203"
+		apiKey: process.env.API_KEY,
+		authDomain: process.env.AUTH_DOMAIN,
+		databaseURL: process.env.DATABASE_URL,
+		storageBucket: process.env.STORAGE_BUCKET,
+		messagingSenderId: process.env.MESSAGING_SENDER_ID
 	};
 
 	firebase.initializeApp(config);
